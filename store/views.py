b2fcbb4ec885d53e87e8a4955ad574e534230345
Filip Van Elsande <filip.vanelslande@gmail.com>
2024-10-15 08:06:11 +0000

    Add Register template & functionality

--- store/views.py
+++ store/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render, redirect
 from .models import Product
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
+from django import forms
 
 def home(request):
 	products = Product.objects.all()
 	return render(request, 'home.html', {'products':products})
-
 
 def about(request):
 	return render(request, 'about.html', {})	
@@ -23,6 +26,7 @@ def login_user(request):
 		else:
 			messages.success(request, ("There was an error, please try again..."))
 			return redirect('login')
+
 	else:
 		return render(request, 'login.html', {})
 
@@ -30,3 +34,22 @@ def logout_user(request):
 	logout(request)
 	messages.success(request, ("You have been logged out...Thanks for stopping by..."))
 	return redirect('home')
+
+def register_user(request):
+	form = SignUpForm()
+	if request.method == "POST":
+		form = SignUpForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password1']
+			# log in user
+			user = authenticate(username=username, password=password)
+			login(request, user)
+			messages.success(request, ("You Have Registered Successfully!! Welcome!"))
+			return redirect('home')
+		else:
+			messages.success(request, ("Whoops! There was a problem Registering, please try again..."))
+			return redirect('register')
+	else:
+		return render(request, 'register.html', {'form':form})
