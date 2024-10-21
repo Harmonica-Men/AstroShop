549f803784b68212f4aceafe8b1e00b942d04263
Filip Van Elsande <filip.vanelslande@gmail.com>
2024-10-21 18:37:22 +0000

    Add 'STATICFILES & DIRECTORY & ROOT' parameters to settings.py

--- shopper/settings.py
+++ shopper/settings.py
@@ -146,7 +146,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = ['static/']
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+# STATICFILES_DIRS = ['static/']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
