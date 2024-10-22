5e4f4767ded182295f20db8ee025ec1ad73be366
Filip Van Elsande <filip.vanelslande@gmail.com>
2024-10-22 16:54:42 +0000

    Fix settings AWS new account

--- shopper/settings.py
+++ shopper/settings.py
@@ -145,8 +145,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # AWS settings
 if 'USE_AWS' in os.environ:
+    print('using aws')
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'aws-astroshop'
+    AWS_STORAGE_BUCKET_NAME = 'astroshop-aws-bucket'
     AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
