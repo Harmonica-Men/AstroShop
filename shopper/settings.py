6b5c46ce31bb2d70001ab204ab102678766a5abb
Filip Van Elsande <filip.vanelslande@gmail.com>
2024-10-25 19:23:28 +0000

    create payment app

--- shopper/settings.py
+++ shopper/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'store',
     'shopcart',
     'storages',
+    'payment',
 ]
 
 MIDDLEWARE = [
--- shopper/urls.py
+++ shopper/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
     path('shopcart/', include('shopcart.urls')),
+    path('payment/', include('payment.urls')),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
