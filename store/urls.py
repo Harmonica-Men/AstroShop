cebe98e80cf29c1a9ae13fb83b1155d49a0b6ac9
Filip Van Elsande <filip.vanelslande@gmail.com>
2024-10-25 11:54:53 +0000

    Add search functionality

--- store/urls.py
+++ store/urls.py
@@ -13,5 +13,6 @@ urlpatterns = [
     path('update_user/', views.update_user, name='update_user'),
     path('update_password/', views.update_password, name='update_password'),    
     path('update_profile/', views.update_profile, name='update_profile'),
+    path('search/', views.search, name='search'),
 ]
 
