88c3ee7721946fefc1ea6c429c428da29c01368b
Filip Van Elsande <filip.vanelslande@gmail.com>
2024-10-25 20:50:07 +0000

    Restore commit 42b797a OK

--- store/views.py
+++ store/views.py
@@ -33,24 +33,14 @@ def search(request):
 
 def update_profile(request):
 	if request.user.is_authenticated:
-		# Get Current User
 		current_user = Profile.objects.get(user__id=request.user.id)
-		# Get Current User's Shipping Info
-		shipping_user = ShippingAddress.objects.get(id=request.user.id)
-		
-		# Get original User Form
 		form = UserInfoForm(request.POST or None, instance=current_user)
-		# Get User's Shipping Form
-		shipping_form = ShippingForm(request.POST or None, instance=shipping_user)		
-		if form.is_valid() or shipping_form.is_valid():
-			# Save original form
-			form.save()
-			# Save shipping form
-			shipping_form.save()
 
+		if form.is_valid():
+			form.save()
 			messages.success(request, "Your Info Has Been Updated!!")
 			return redirect('home')
-		return render(request, "update_info.html", {'form':form, 'shipping_form':shipping_form})
+		return render(request, "update_profile.html", {'form':form})
 	else:
 		messages.success(request, "You Must Be Logged In To Access That Page!!")
 		return redirect('home')
