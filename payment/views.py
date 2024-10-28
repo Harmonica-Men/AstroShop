3833b4fa20886a6d61288d9da32f49b40ea6ca15
Filip Van Elsande <filip.vanelslande@gmail.com>
2024-10-28 12:31:21 +0000

    Add order items

--- payment/views.py
+++ payment/views.py
@@ -4,6 +4,8 @@ from payment.forms import ShippingForm, PaymentForm
 from payment.models import ShippingAddress, Order, OrderItem
 from django.contrib.auth.models import User
 from django.contrib import messages
+from store.models import Product
+
 
 def process_order(request):
 	if request.POST:
@@ -33,14 +35,62 @@ def process_order(request):
 			create_order = Order(user=user, full_name=full_name, email=email, shipping_address=shipping_address, amount_paid=amount_paid)
 			create_order.save()
 
+			# Add order items
+			
+			# Get the order ID
+			order_id = create_order.pk
+			
+			# Get product Info
+			for product in cart_products():
+				# Get product ID
+				product_id = product.id
+				# Get product price
+				if product.is_sale:
+					price = product.sale_price
+				else:
+					price = product.price
+
+				# Get quantity
+				for key,value in quantities().items():
+					if int(key) == product.id:
+						# Create order item
+						create_order_item = OrderItem(order_id=order_id, product_id=product_id, user=user, quantity=value, price=price)
+						create_order_item.save()
+
 			messages.success(request, "Order Placed!")
 			return redirect('home')
+
+			
 
 		else:
 			# not logged in
 			# Create Order
 			create_order = Order(full_name=full_name, email=email, shipping_address=shipping_address, amount_paid=amount_paid)
 			create_order.save()
+
+			# Add order items
+			
+			# Get the order ID
+			order_id = create_order.pk
+			
+			# Get product Info
+			for product in cart_products():
+				# Get product ID
+				product_id = product.id
+				# Get product price
+				if product.is_sale:
+					price = product.sale_price
+				else:
+					price = product.price
+
+				# Get quantity
+				for key,value in quantities().items():
+					if int(key) == product.id:
+						# Create order item
+						create_order_item = OrderItem(order_id=order_id, product_id=product_id, quantity=value, price=price)
+						create_order_item.save()
+
+
 
 			messages.success(request, "Order Placed!")
 			return redirect('home')
@@ -57,6 +107,7 @@ def billing_info(request):
 		cart_products = cart.get_prods
 		quantities = cart.get_quants
 		totals = cart.cart_total()
+
 		# Create a session with Shipping Info
 		my_shipping = request.POST
 		request.session['my_shipping'] = my_shipping
@@ -74,6 +125,7 @@ def billing_info(request):
 			return render(request, "payment/billing_info.html", {"cart_products":cart_products, "quantities":quantities, "totals":totals, "shipping_info":request.POST, "billing_form":billing_form})
 
 
+		
 		shipping_form = request.POST
 		return render(request, "payment/billing_info.html", {"cart_products":cart_products, "quantities":quantities, "totals":totals, "shipping_form":shipping_form})	
 	else:
@@ -99,6 +151,8 @@ def checkout(request):
 		# Checkout as guest
 		shipping_form = ShippingForm(request.POST or None)
 		return render(request, "payment/checkout.html", {"cart_products":cart_products, "quantities":quantities, "totals":totals, "shipping_form":shipping_form})
+
 	
+
 def payment_success(request):
 	return render(request, "payment/payment_success.html", {})
