7d4be138775eed229741d724e977505d66640518
Filip Van Elsande <filip.vanelslande@gmail.com>
2024-11-05 17:55:28 +0000

    Add template include directory

--- shopper/settings.py
+++ shopper/settings.py
@@ -9,10 +9,10 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
-import os
+
 import dj_database_url
 if os.path.isfile('env.py'):
     import env
@@ -20,7 +20,7 @@ if os.path.isfile('env.py'):
 
 # settings.py
 
-import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -102,7 +102,13 @@ ROOT_URLCONF = 'shopper.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "templates"],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
+
+        # 'DIRS': [BASE_DIR / "templates"],
+
+
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,6 +121,9 @@ TEMPLATES = [
         },
     },
 ]
+
+SITE_ID = 1
+
 
 WSGI_APPLICATION = 'shopper.wsgi.application'
 
