bb15971286a40e5f409f39e80b6fdc6771cdaede
Filip Van Elsande <filip.vanelslande@gmail.com>
2024-11-06 17:35:23 +0000

    Added sorting JS and back top link

--- store/models.py
+++ store/models.py
@@ -58,7 +58,8 @@ class Customer(models.Model):
 class Product(models.Model):
 	name = models.CharField(max_length=100)
 	price = models.DecimalField(max_digits=10, decimal_places=2)
-	category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+	# category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+	category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
 	description = models.TextField(default='', blank=True, null=True)
 	image = models.ImageField(upload_to='uploads/product/')
 	# Add Sale Stuff
--- store/views.py
+++ store/views.py
@@ -6,6 +6,9 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .forms import SignUpForm, UpdateUserForm, ChangePasswordForm, UserInfoForm, UpdateProductForm
 
+from django.db.models.functions import Lower
+
+
 from payment.forms import ShippingForm
 from payment.models import ShippingAddress
 
@@ -37,37 +40,26 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-
+            if sortkey == 'category':
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
             
-        # if 'category' in request.GET:
-        #     categories = request.GET['category'].split(',')
-        #     products = products.filter(category__name__in=categories)
-        #     categories = Category.objects.filter(name__in=categories)
-
-        # if 'q' in request.GET:
-        #     query = request.GET['q']
-        #     if not query:
-        #         messages.error(request, "You didn't enter any search criteria!")
-        #         return redirect(reverse('products'))
-            
-        #     queries = Q(name__icontains=query) | Q(description__icontains=query)
-        #     products = products.filter(queries)
-
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+        
     current_sorting = f'{sort}_{direction}'
 
     context = {
         'products': products,
-        'search_term': query,
         'current_categories': categories,
         'current_sorting': current_sorting,
 	  }
-
-
 
     return render(request, 'products.html', context)
 
