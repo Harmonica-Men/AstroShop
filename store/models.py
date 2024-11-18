4937101ec2916900f0c280ffb3a6256e6dfe4ff7
Filip Van Elsande <filip.vanelslande@gmail.com>
2024-11-18 18:18:03 +0000

    remove orders models.py store & customers

--- store/models.py
+++ store/models.py
@@ -40,18 +40,6 @@ class Category(models.Model):
 		verbose_name_plural = 'categories'
 
 
-# Customers
-class Customer(models.Model):
-	first_name = models.CharField(max_length=50)
-	last_name = models.CharField(max_length=50)
-	phone = models.CharField(max_length=10)
-	email = models.EmailField(max_length=100)
-	password = models.CharField(max_length=100)
-
-
-	def __str__(self):
-		return f'{self.first_name} {self.last_name}'
-
 
 
 # All of our Products
@@ -69,21 +57,6 @@ class Product(models.Model):
 	def __str__(self):
 		return self.name		
 	
-
-
-# Customer Orders
-class Order(models.Model):
-	product = models.ForeignKey(Product, on_delete=models.CASCADE)
-	customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-	quantity = models.IntegerField(default=1)
-	address = models.CharField(max_length=100, default='', blank=True)
-	phone = models.CharField(max_length=20, default='', blank=True)
-	date = models.DateField(default=datetime.datetime.today)
-	status = models.BooleanField(default=False)
-
-	def __str__(self):
-		return self.product
-
 
 class Subscription(models.Model):
     email = models.EmailField(unique=True)
