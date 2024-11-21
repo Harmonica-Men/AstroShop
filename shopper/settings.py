6b4f0caa794a313c73be1bdfb9f5c993d823ab2d
Filip Van Elsande <filip.vanelslande@gmail.com>
2024-11-21 19:02:02 +0000

    Fix email settings

--- shopper/settings.py
+++ shopper/settings.py
@@ -201,11 +201,11 @@ PAYPAL_TEST = True
 
 PAYPAL_RECEIVER_EMAIL = 'test-business-paypal@vanelslande.com' # Business Sandbox test account
 
-
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASS")
-DEFAULT_FROM_EMAIL = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_USER = 'filip.vanelslande@gmail.com'
+EMAIL_HOST_PASSWORD = 'auhb luzi lizg caqx '
+DEFAULT_FROM_EMAIL = 'filip.vanelslande@gmail.com'
+# DEFAULT_FORM_EMAIL = os.getenv('EMAIL_HOST_USER')
