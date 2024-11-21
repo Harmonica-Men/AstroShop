39fd8422c360aeb2c47b6024ba25f45dc2eda9a5
Filip Van Elsande <filip.vanelslande@gmail.com>
2024-11-21 19:34:18 +0000

    test email settings part 3

--- shopper/settings.py
+++ shopper/settings.py
@@ -205,7 +205,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_USER = 'filip.vanelslande@gmail.com'
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 DEFAULT_FORM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
