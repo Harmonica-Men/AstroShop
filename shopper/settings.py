381ce95cf75d1eca01301bea422bc430989340f8
Filip Van Elsande <filip.vanelslande@gmail.com>
2024-11-21 18:51:17 +0000

    Fix EMAIL_HOST_PASSWORD

--- shopper/settings.py
+++ shopper/settings.py
@@ -207,5 +207,5 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'filip.vanelslande@gmail.com'
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_PASSWORD = os.getenv('auhb luzi lizg caqx ')
 DEFAULT_FORM_EMAIL = 'filip.vanelslande@gmail.com'
