806d79fd9f8046797e97c228e146a5ecf99d6399
Filip Van Elsande <filip.vanelslande@gmail.com>
2024-11-21 19:53:51 +0000

    Renew email settings

--- shopper/settings.py
+++ shopper/settings.py
@@ -205,8 +205,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'filip.vanelslande@gmail.com'
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
-DEFAULT_FORM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+DEFAULT_FORM_EMAIL = env('EMAIL_HOST_USER')
 
 
