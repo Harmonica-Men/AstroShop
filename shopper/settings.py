7b785db8bd728ce5d03da703ec4aec6df81b3448
Filip Van Elsande <filip.vanelslande@gmail.com>
2024-11-21 11:34:45 +0000

    Fix bug settings.py email backend SMTP

--- shopper/settings.py
+++ shopper/settings.py
@@ -197,7 +197,7 @@ PAYPAL_RECEIVER_EMAIL = 'test-business-paypal@vanelslande.com' # Business Sandbo
 
 DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER')
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
