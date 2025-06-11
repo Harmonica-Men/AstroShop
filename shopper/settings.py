bb037c32f8786c6df8c8d9a6069ff4e694e67f79
Filip Van Elslande <filip.vanelslande@gmail.com>
2025-06-11 14:50:30 +0000

    Enable debug troubleshooting email

--- shopper/settings.py
+++ shopper/settings.py
@@ -31,12 +31,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 CLOUDINARY_URL = os.environ.get("CLOUDINARY_URL")
-DEBUG = False
-CSRF_TRUSTED_ORIGINS = ['https://8000-harmonicamen-astroshop-37z6o1iwvkr.ws.codeinstitute-ide.net']  # noqa
+DEBUG = True
+# CSRF_TRUSTED_ORIGINS = ['https://8000-harmonicamen-astroshop-37z6o1iwvkr.ws.codeinstitute-ide.net']  # noqa
+CSRF_TRUSTED_ORIGINS = ['https://8000-harmonicamen-astroshop-znivqq343gt.ws-eu120.gitpod.io']
+## ALLOWED_HOSTS = [
+##    '8000-harmonicamen-astroshop-37z6o1iwvkr.ws.codeinstitute-ide.net',
+##    '.herokuapp.com'
+## ]
 ALLOWED_HOSTS = [
-    '8000-harmonicamen-astroshop-37z6o1iwvkr.ws.codeinstitute-ide.net',
+    '8000-harmonicamen-astroshop-znivqq343gt.ws-eu120.gitpod.io',
     '.herokuapp.com'
 ]
+
+
 
 # Application definition
 INSTALLED_APPS = [
--- store/migrations/0014_category_slug.py
+++ store/migrations/0014_category_slug.py
@@ -13,6 +13,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='category',
             name='slug',
-            field=models.SlugField(default='default-slug', unique=True),
+            ## field=models.SlugField(default='default-slug', unique=True),
+            field=models.SlugField(default='default-slug'),
         ),
     ]
